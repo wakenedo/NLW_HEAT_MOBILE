fa0afb3e1b29d2a796fd2d9cb5fef2d8cbb21816
wakenedo <a_nuner@outlook.com>
2021-10-24 20:19:48 -0300

    Setup Files and Assets
    React Native, Expo & Android Studio

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+import LogoSvg from '../../assets/logo.svg';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+
+            <LogoSvg />            
+            <Text>Sair</Text>
+            
+        </View>
+    )
+}
+
+
