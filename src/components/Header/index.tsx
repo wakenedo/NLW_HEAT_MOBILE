007dd6f0d38fb8e025eb2db0680fa6a704d7c2a8
wakenedo <a_nuner@outlook.com>
2021-10-24 21:16:26 -0300

    Components & Styling - Part 1
    Header Component + SVG elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-
+import { Text, View, TouchableOpacity } from 'react-native';
+ 
 import { styles } from './styles';
-
 import LogoSvg from '../../assets/logo.svg';
+import { UserPhoto } from '../UserPhoto';
 
 export function Header() {
     return (
         <View style={styles.container}>
+            <LogoSvg /> 
 
-            <LogoSvg />            
-            <Text>Sair</Text>
+            <View style={styles.logoutButton}>
+                <TouchableOpacity>
+                    <Text style={styles.logoutText}>Sair</Text>
+                </TouchableOpacity>
+
+             <UserPhoto 
+                imageUri={'https://github.com/wakenedo.png'}         
+              />
+            </View>
+
+            
+                         
             
         </View>
     )
